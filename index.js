0a091fb2d36e4d6f826afe97ed4ad7aa2e8f77c2
TamasParkany <thomasovmail@gmail.com>
2022-11-03 21:15:59 +0100

    Added shopping cart

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
+//CART-OVERLAY
+const cartOverlay = document.getElementsByClassName("cart-overlay")[0];
+const cartOpen = document.getElementsByClassName("cart")[0];
+const cartClose = document.getElementsByClassName("cart-close")[0];
+const cartFinish = document.getElementsByClassName("cart-finish")[0];
+const cartContinue = document.getElementsByClassName("cart-continue")[0];
+
+cartOpen.addEventListener("click", () => {
+  cartOverlay.style.left = "0";
+  document.body.classList.add("stop-scrolling");
+});
+cartClose.addEventListener("click", () => {
+  cartOverlay.style.left = "-100vw";
+  document.body.classList.remove("stop-scrolling");
+});
+cartFinish.addEventListener("click", () => {
+  console.log("Data sent to Stripe");
+});
+cartContinue.addEventListener("click", () => {
+  cartOverlay.style.left = "-100vw";
+  document.body.classList.remove("stop-scrolling");
+});
+
 //NAVBAR-OVERLAY
 const navOverlay = document.getElementsByClassName("nav-overlay")[0];
 const navOpen = document.getElementsByClassName("hamburger")[0];
@@ -19,7 +42,9 @@ const navClose = document.getElementsByClassName("nav-close")[0];
 
 navOpen.addEventListener("click", () => {
   navOverlay.style.right = "0";
+  document.body.classList.add("stop-scrolling");
 });
 navClose.addEventListener("click", () => {
   navOverlay.style.right = "-100vw";
+  document.body.classList.remove("stop-scrolling");
 });
--- shop/product.js
+++ shop/product.js
@@ -12,6 +12,29 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
+//CART-OVERLAY
+const cartOverlay = document.getElementsByClassName("cart-overlay")[0];
+const cartOpen = document.getElementsByClassName("cart")[0];
+const cartClose = document.getElementsByClassName("cart-close")[0];
+const cartFinish = document.getElementsByClassName("cart-finish")[0];
+const cartContinue = document.getElementsByClassName("cart-continue")[0];
+
+cartOpen.addEventListener("click", () => {
+  cartOverlay.style.left = "0";
+  document.body.classList.add("stop-scrolling");
+});
+cartClose.addEventListener("click", () => {
+  cartOverlay.style.left = "-100vw";
+  document.body.classList.remove("stop-scrolling");
+});
+cartFinish.addEventListener("click", () => {
+  console.log("Data sent to Stripe");
+});
+cartContinue.addEventListener("click", () => {
+  cartOverlay.style.left = "-100vw";
+  document.body.classList.remove("stop-scrolling");
+});
+
 //NAVBAR-OVERLAY
 const navOverlay = document.getElementsByClassName("nav-overlay")[0];
 const navOpen = document.getElementsByClassName("hamburger")[0];
@@ -19,9 +42,11 @@ const navClose = document.getElementsByClassName("nav-close")[0];
 
 navOpen.addEventListener("click", () => {
   navOverlay.style.right = "0";
+  document.body.classList.add("stop-scrolling");
 });
 navClose.addEventListener("click", () => {
   navOverlay.style.right = "-100vw";
+  document.body.classList.remove("stop-scrolling");
 });
 
 //SIZE-GUIDE
@@ -79,9 +104,11 @@ const sleeve = document.getElementById("sleeve");
 
 sizeGuideOpen.addEventListener("click", () => {
   sizeGuideOverlay.style.display = "block";
+  document.body.classList.add("stop-scrolling");
 });
 sizeGuideClose.addEventListener("click", () => {
   sizeGuideOverlay.style.display = "none";
+  document.body.classList.remove("stop-scrolling");
 });
 
 function sizeGuideSelect(e) {
