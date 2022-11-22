e8818a5c92850b213d663ca3b3a55a7c39e8d162
TamasParkany <thomasovmail@gmail.com>
2022-11-22 20:32:43 +0100

    fixes in shop page

--- shop/shop.js
+++ shop/shop.js
@@ -1,26 +1,3 @@
-const image1 = document.getElementById("image1");
-const image2 = document.getElementById("image2");
-const image3 = document.getElementById("image3");
-
-image1.addEventListener("mouseover", () => {
-  image1.src = "/assets/images/real/real_front.JPG";
-});
-image1.addEventListener("mouseout", () => {
-  image1.src = "/assets/images/real/real_group.JPG";
-});
-image2.addEventListener("mouseover", () => {
-  image2.src = "/assets/images/slasher/slasher_front_f.JPG";
-});
-image2.addEventListener("mouseout", () => {
-  image2.src = "/assets/images/slasher/slasher_group_alt.JPG";
-});
-image3.addEventListener("mouseover", () => {
-  image3.src = "/assets/images/lost/lost_front_f.JPG";
-});
-image3.addEventListener("mouseout", () => {
-  image3.src = "/assets/images/lost/lost_group_alt.JPG";
-});
-
 // COUNTDOWN
 const countDownDate = new Date("Dec 2, 2022 19:00:00").getTime();
 let prevMinutes = null;
@@ -68,3 +45,35 @@ const x = setInterval(function () {
     console.log("countdown expired");
   }
 }, 1000);
+
+//IMAGE-MOUSEOVER
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  console.log("");
+} else {
+  const image1 = document.getElementById("image1");
+  const image2 = document.getElementById("image2");
+  const image3 = document.getElementById("image3");
+
+  image1.addEventListener("mouseover", () => {
+    image1.src = "/assets/images/real/real_front.JPG";
+  });
+  image1.addEventListener("mouseout", () => {
+    image1.src = "/assets/images/real/real_group.JPG";
+  });
+  image2.addEventListener("mouseover", () => {
+    image2.src = "/assets/images/slasher/slasher_front_f.JPG";
+  });
+  image2.addEventListener("mouseout", () => {
+    image2.src = "/assets/images/slasher/slasher_group_alt.JPG";
+  });
+  image3.addEventListener("mouseover", () => {
+    image3.src = "/assets/images/lost/lost_front_f.JPG";
+  });
+  image3.addEventListener("mouseout", () => {
+    image3.src = "/assets/images/lost/lost_group_alt.JPG";
+  });
+}
